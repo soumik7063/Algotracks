2b8995dfe68266981e166f3daaa911759ddf1f76
soumik7063 <ghataksoumik7063@gmail.com>
2025-08-09 00:17:46 +0530

    Navbar update

--- algotracks/src/components/layout/Navbar.jsx
+++ algotracks/src/components/layout/Navbar.jsx
@@ -15,9 +15,10 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo and site name */}
-          <div className="flex-shrink-0 flex items-center">
-            <Link to="/" className="font-bold text-xl">
-              CP Helper
+          <div className="flex-shrink-0 flex flex-row items-center">
+            <Link to="/" className="font-bold text-xl flex gap-2 items-center">
+            <img className="h-10 w-10 " src="./logo.png" alt="" />
+              AlgoTracks
             </Link>
           </div>
 
