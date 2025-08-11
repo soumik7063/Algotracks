e6b804528a8ec0901a6cddc525882e7c46e63805
soumik7063 <ghataksoumik7063@gmail.com>
2025-08-12 00:09:23 +0530

    profile context added

---
+++ algotracks/src/components/profile/ProfileContext.jsx
@@ -0,0 +1,110 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { getUserProfile, getRecentSubmissions } from '../../services/Leetcode';
+import { AuthContext } from "../../AuthContext";
+
+export const Profilecontext = createContext(null)
+
+const ProfileContext = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    const LeetcodeUsername = user?.cpProfiles?.Leetcode;
+    const CodeforceUsername = user?.cpProfiles?.Leetcode;
+      const [codeforceData, setCodeforceDta] = useState(null);
+    const [leetcodeData, setLeetcodeData] = useState(null);
+    const [submissions, setSubmissions] = useState([]); 
+    const [CodeforceSubmission, setCodeforceSubmission] = useState([]); 
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(null); 
+    const CodeforceLastSubmissionfetch = async()=>{
+      if (!CodeforceUsername) {
+        setIsError("No LeetCode username found in user profile");
+        setIsLoading(false);
+        return;
+    }
+    try {
+      const lastsubmit = await fetch(`https://codeforces.com/api/user.status?handle=${CodeforceUsername}&from=1&count=10`)
+      if(lastsubmit && lastsubmit.status === "OK"){
+        setCodeforceSubmission(lastsubmit.result);
+      }
+    } catch (error) {
+      console.error('Error fetching LeetCode data:', error);
+            setIsError("Not finding user please check username");
+            setCodeforceSubmission([]);
+    }
+    }
+    const handleSearch = async () => {
+        if (!LeetcodeUsername) {
+            setIsError("No LeetCode username found in user profile");
+            setIsLoading(false);
+            return;
+        }
+
+        try {
+            setIsLoading(true);
+            setIsError(null); 
+            
+            const submissionsData = await getRecentSubmissions(LeetcodeUsername, 100); 
+            const data = await submissionsData.json()
+            setSubmissions(submissionsData.data); 
+            const ProfileInfo = await getUserProfile(LeetcodeUsername);
+            setLeetcodeData(ProfileInfo.data);
+        } catch (error) {
+            console.error('Error fetching LeetCode data:', error);
+            setIsError("Not finding user please check username");
+            setLeetcodeData(null);
+            setSubmissions([]);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    const getUser = async () => {
+      setIsLoading(true);
+      setIsError(null);
+      try {
+        const response = await fetch(
+          `https://codeforces.com/api/user.info?handles=${CodeforceUsername}`
+        );
+        const data = await response.json();
+
+        if (data.status == "OK") {
+          setCodeforceDta(data.result[0]);
+        } else {
+          setIsError("Codeforce User not found. Please check the username and try again.");
+        }
+      } catch (error) {
+        setIsError("Failed to fetch user data. Please try again later.");
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    useEffect(() => {
+        if (LeetcodeUsername && leetcodeData === null && submissions.length === 0 && !isError) {
+            handleSearch();
+        }
+    }, [LeetcodeUsername]);
+    useEffect(()=>{
+        if(CodeforceUsername && codeforceData === null && CodeforceSubmission.length === 0 && !isError){
+            getUser();
+            // CodeforceLastSubmissionfetch();
+        }
+    },[CodeforceUsername])
+    const contextValue = {
+        leetcodeData,
+        submissions,
+        isLoading,
+        isError,
+        LeetcodeUsername,
+        handleSearch,
+        codeforceData,
+        CodeforceSubmission,
+        refetch: handleSearch 
+    };
+
+    return (
+        <Profilecontext.Provider value={contextValue}>
+            {children}
+        </Profilecontext.Provider>
+    )
+}
+
+export default ProfileContext
