d183093aae5bed4c152ae9e6076cf0f2913082b4
soumik7063 <ghataksoumik7063@gmail.com>
2026-01-05 12:34:36 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'https://algotracks-frontend-1.onrender.com',
-    'https://algotracks.vercel.app',
+    'https://algotracks-cp-helper.vercel.app',
     'http://localhost:5173' // for local development
   ],
   credentials: true
